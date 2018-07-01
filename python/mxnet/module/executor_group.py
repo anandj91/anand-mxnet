de398f1ff6c3b451df4f7267826a8e091dc34e85
Anand J <anand.indukala@gmail.com>
2018-07-01 01:53:35 -0400

    Param logging in the order

--- python/mxnet/module/executor_group.py
+++ python/mxnet/module/executor_group.py
@@ -396,16 +396,15 @@ class DataParallelExecutorGroup(object):
         """
         arg_size = 0
         aux_size = 0
-        for arg_param, arg_arr in arg_params.items():
-            arg_size += arg_arr.size
-            self.logger.info("ARG %s = %d (%s)" % (arg_param, arg_arr.size, arg_arr.dtype))
-
-        for aux_param, aux_arr in aux_params.items():
-            aux_size += aux_arr.size
-            self.logger.info("AUX %s = %d (%s)" % (aux_param, aux_arr.size, aux_arr.dtype))
+        for arg_param in self.arg_names:
+            try:
+                arg_arr = arg_params[arg_param]
+                arg_size += arg_arr.size
+                self.logger.info("ARG %s = %d (%s)" % (arg_param, arg_arr.size, arg_arr.dtype))
+            except KeyError:
+                pass
 
         self.logger.info("Total arg_params size = %d" % arg_size)
-        self.logger.info("Total aux_params size = %d" % aux_size)
 
         for exec_ in self.execs:
             exec_.copy_params_from(arg_params, aux_params, allow_extra_params=allow_extra)
