a803c491cf4ea4498f2846d3cf5491491bdd0799
Anand J <anandj@cs.ubc.ca>
2019-06-18 14:52:00 -0700

    Encode at init

--- src/kvstore/kvstore_dist.h
+++ src/kvstore/kvstore_dist.h
@@ -192,6 +192,9 @@ class KVStoreDist : public KVStoreLocal {
     CheckUnique(keys);
     for (size_t i = 0; i < keys.size(); ++i) {
       comm_->Init(keys[i], values[i].storage_type(), values[i].shape(), values[i].dtype());
+      //TODO: Comment
+      EncodePriorityKey(keys[i], values[i].shape().Size(),
+              mshadow::mshadow_sizeof(values[i].dtype()));
     }
     if (get_rank() == 0 && this->ps_worker_->get_customer()->customer_id() == 0) {
       Push_(keys, values, 0, false);
